3665d6a4d4917a2946f9f1388bd7181438766aad
zalazuzek <44084668+zalazuzek@users.noreply.github.com>
2018-10-12 12:48:54 +0200

    Update Lab_1.R

---
+++ Lab_1.R
@@ -0,0 +1,22 @@
+ x<-(1:100)
+> print(x)
+
+ list(1:10)
+ 
+ matrix(1:100, nrow=10, ncol=5)
+ 
+ x<-c("banana","jagoda","marelica","sliva","ananas")
+  z = factor(x, order = TRUE)
+  z
+ 
+  x<-c(1000:1050)
+  y<-c(50:100)
+  z<-c(51:101)
+  n<-c(55:105)
+  mydf= data.frame(x,y,z,n)
+  colnames(mydf)=c("stolpec1","stolpec2","stolpec3","stolpec4")
+  mydf
+  
+  
+  
+  
